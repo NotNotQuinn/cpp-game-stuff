4d288b5d26ea64cd16fc2948752b6bdbb4c3a4f9
Quinn T <Quinn.GitHub@gmail.com>
2021-01-13 16:10:39 -0600

    idk, hacks doesnt work because utf-8 ist supported

--- hacks.cpp
+++ hacks.cpp
@@ -3,14 +3,12 @@ using namespace std;  // my intellesence is being dumb
 
 int main()
 {
-    _setmode(_fileno(std::stdout), _O_U8TEXT);
-    cout << "OH NO!! youve been hacked!!" << endl;
-    cout << endl << "Here are you options:" << endl;
-    cout << endl << "    Press [1] to get hacked." << endl;
-    cout << "    Press [2] to not get hacked." << endl;
+    cout <<         "OH NO!! youve been hacked!!"      << endl;
+    cout << endl << "Here are you options:"            << endl;
+    
+    cout << endl << "    Press [1] to get hacked."     << endl;
+    cout <<         "    Press [2] to not get hacked." << endl;
     unsigned long long a = 0;
-    cin >> a;
-    cout << endl << "too late bucko, I win." << endl << endl;
     cin >> a;
     cout << "⣿⣿⣿⣿⣿⣿⠟⢉⣤⣴⣶⣶⣦⣬⣙⠻⠛⣉⣥⣤⣤⣬⡙⠿⣿⣿⣿⣿⣿" << endl
          << "⣿⣿⣿⣿⠟⢁⣴⣿⣿⡿⢿⣟⣛⠿⢿⣧⢻⣿⣿⣿⣿⣿⣿⣆⠹⣿⣿⣿⣿" << endl
