190db2466f2fff2b98067166deef194bb3a2207b
Quinn T <Quinn.github@gmail.com>
2021-02-02 18:14:16 -0600

    cleanup, and move random files to there own directory

---
+++ randomstuffs/hacks.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;  // my intellesence is being dumb
+
+int main()
+{
+    cout <<         "OH NO!! youve been hacked!!"      << endl;
+    cout << endl << "Here are you options:"            << endl;
+    
+    cout << endl << "    Press [1] to get hacked."     << endl;
+    cout <<         "    Press [2] to not get hacked." << endl;
+    unsigned long long a = 0;
+    cin >> a;
+    cout << "⣿⣿⣿⣿⣿⣿⠟⢉⣤⣴⣶⣶⣦⣬⣙⠻⠛⣉⣥⣤⣤⣬⡙⠿⣿⣿⣿⣿⣿" << endl
+         << "⣿⣿⣿⣿⠟⢁⣴⣿⣿⡿⢿⣟⣛⠿⢿⣧⢻⣿⣿⣿⣿⣿⣿⣆⠹⣿⣿⣿⣿" << endl
+         << "⣿⣿⣿⠏⣰⣿⣿⣯⣷⣿⣿⣟⣛⣛⡻⠾⣍⢽⣷⣶⣶⠶⣯⡭⢤⣉⠛⢿⣿" << endl
+         << "⣿⠟⢛⢰⣿⣿⣿⣿⡿⢿⣛⢝⣢⣭⣭⣭⣭⣓⣁⠊⣉⣒⣤⣤⣤⣴⣃⡀⠙" << endl
+         << "⠃⣴⣿⣼⣿⣿⣿⠫⠭⡽⢞⣻⣭⣭⠭⠍⠭⠽⢻⣿⣻⠫⠽⠿⠶⠖⣐⣾⡅" << endl
+         << "⣴⣿⣿⣿⣿⣿⣿⣿⣴⡿⠿⣯⣉⣁⠄⠄⢒⣩⠏⠙⠛⠄⠄⠒⠒⠤⡾⠛⢁" << endl
+         << "⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠯⠥⠶⠖⠚⠛⢛⣓⣐⣐⣒⣒⡒⠄⠈⠿⣿" << endl
+         << "⣿⣿⣿⣿⣿⣿⣿⡟⢻⡇⡶⣪⣭⣿⣿⣷⣶⣶⣦⣶⣦⣿⣿⣿⣾⣿⣿⠆⢸" << endl
+         << "⣿⣿⣿⣿⣿⣿⣿⣷⡌⠇⡻⢟⡻⣿⣯⣽⣿⣽⣿⣽⣿⣼⣿⣽⢿⣿⠋⣰⣿" << endl
+         << "⣿⣿⣿⣿⣿⡏⢿⣿⣿⣶⣌⢢⣝⠺⠭⠟⠛⠛⠛⠛⠻⠭⠽⢒⠋⠁⣼⣿⣿" << endl
+         << "⠙⠻⢿⣿⣿⣿⣶⣤⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣶⡶⠚⢁⣤⣴⣿⣿⣿" << endl
+         << "⣿⣦⣄⣂⠨⠍⣉⣛⣛⡛⠛⠛⠻⠿⠿⠟⠛⠛⠛⠉⢡⣴⣾⣿⣿⣿⣿⣿⣿" << endl
+         << "⣿⣿⣿⣿⣿⣷⣶⣶⣶⣦⣬⣭⣭⣭⣭⣭⣤⣶⣿⣶⣄⡙⢿⣿⣿⣿⣿⣿⣿" << endl;
+    cin >> a;
+    cin >> a;
+}
